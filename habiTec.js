bd8fd1a17dcdb9c6064bdc10c1e783b4835f4774
Daniel <daniel.e.farjatt@gmail.com>
2023-02-08 16:47:49 -0300

    agrego el proyecto

---
+++ habiTec.js
@@ -0,0 +1,27 @@
+let rango = document.querySelector("#rango1")
+let result = document.querySelector("#Result1")
+
+rango.onchange = () => {
+    result.innerHTML = rango.value
+}
+
+let rango2 = document.querySelector("#rango2")
+let result2 = document.querySelector("#Result2")
+
+rango2.onchange = () => {
+    result2.innerHTML = rango2.value
+}
+
+let rango3 = document.querySelector("#rango3")
+let result3 = document.querySelector("#Result3")
+
+rango3.onchange = () => {
+    result3.innerHTML = rango3.value
+}
+
+let rango4 = document.querySelector("#rango4")
+let result4 = document.querySelector("#Result4")
+
+rango4.onchange = () => {
+    result4.innerHTML = rango4.value
+}
